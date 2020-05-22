e799f8b834e9df4249c8fcbf00fe02720878fd9d
Welder Rodrigues <welder_15@hotmail.com>
2020-05-22 01:06:40 -0300

    Prettier
    Adicionado as configurações do prettier ao padrão de codigo

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ Message: 'Hello TypeScript Configurado' }));
+app.use(routes);
+
 app.listen(3333, () => {
   console.log('Server Start');
 });
