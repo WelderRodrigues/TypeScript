1d8af980fb20c2fff60cd984c6742d4f9a08d0c9
Welder Rodrigues <welder_15@hotmail.com>
2020-05-21 22:50:38 -0300

    Primeiro projeto node
    Configurando Estrutura do projeto

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ const app = express();
 
 app.get("/", (request, response) => {
   /* response.send("TypeScript"); */
-  return response.json({ Message: "Hello TypeScript" });
+  return response.json({ Message: "Hello TypeScript Configurado" });
 });
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Server Start");
+});
