f42f950ef0ced486d84bc6cc802504499b0924eb
Welder Rodrigues <welder_15@hotmail.com>
2020-06-16 23:46:52 -0300

    Gerando token JWT
    token de autenticação jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
-import { request } from 'express';
 
 interface Request {
   email: string;
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -29,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'fsdfdsfdskhfhadsgfhjadkgshfas', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
